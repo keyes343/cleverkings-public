ae9bc786b3bcd20bdb2b6134d7d300c76d10d110
keyes343 <keyes343@gmail.com>
2021-09-06 14:10:48 +0530

    random

--- components/media/ourClients/index.ts
+++ components/media/ourClients/index.ts
@@ -3,18 +3,18 @@ import clients_world_wild from './clients_world_wild.svg';
 import project_delivered from './project_delivered.svg';
 import years_of_expertise from './years_of_expertise.svg';
 
-import Clients_02 from './clientList/clients_02.svg';
-import Clients_04 from './clientList/clients_04.svg';
-import Clients_05 from './clientList/clients_05.svg';
-import Clients_06 from './clientList/clients_06.svg';
-import Clients_07 from './clientList/clients_07.svg';
-import Clients_08 from './clientList/clients-08.svg';
-import Clients_10 from './clientList/clients-10.svg';
-import Clients_11 from './clientList/clients-11.svg';
-import Clients_12 from './clientList/clients-12.svg';
-import Clients_13 from './clientList/clients-13.svg';
-import Clients_14 from './clientList/clients-14.svg';
-import Clients_15 from './clientList/clients-15.svg';
+import Clients_02 from './clientList/Clients_02.svg';
+import Clients_04 from './clientList/Clients_04.svg';
+import Clients_05 from './clientList/Clients_05.svg';
+import Clients_06 from './clientList/Clients_06.svg';
+import Clients_07 from './clientList/Clients_07.svg';
+import Clients_08 from './clientList/Clients-08.svg';
+import Clients_10 from './clientList/Clients-10.svg';
+import Clients_11 from './clientList/Clients-11.svg';
+import Clients_12 from './clientList/Clients-12.svg';
+import Clients_13 from './clientList/Clients-13.svg';
+import Clients_14 from './clientList/Clients-14.svg';
+import Clients_15 from './clientList/Clients-15.svg';
 
 export {
     awesome_people,
